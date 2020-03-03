e92fd32a9395195c622c428fee8b4e2523517af3
jeff007ali <jagharia@gmail.com>
2020-03-03 23:50:27 +0530

    Added code to create custom hotkeys for uppercase and lowercase

---
+++ custom-hotkeys.py
@@ -0,0 +1,72 @@
+from pynput.keyboard import Key, Controller, KeyCode, Listener
+import pyperclip
+import time
+
+def copy_text():
+    # print('copy')
+    keyboard = Controller()
+    keyboard.press(Key.ctrl_l)
+    keyboard.press('c')
+    keyboard.release('c')
+    keyboard.release(Key.ctrl_l)
+    time.sleep(0.1)
+
+def paste_text():
+    # print('paste')
+    keyboard = Controller()
+    keyboard.press(Key.ctrl_l)
+    keyboard.press('v')
+    keyboard.release('v')
+    keyboard.release(Key.ctrl_l)
+
+def convert_to_uppercase():
+    # print("In uppercase function")
+    data = pyperclip.paste()
+    # print('data before copy : {}'.format(data))
+    copy_text()
+    data = pyperclip.paste()
+    # print('data after copy : {}'.format(data))
+
+    data = data.upper()
+
+    # print('modified data before paste : {}'.format(data))
+    pyperclip.copy(data)
+    paste_text()
+    # print('modified data after paste : {}'.format(data))
+
+def convert_to_lowercase():
+    # print("In lowercase function")
+    data = pyperclip.paste()
+    # print('data before copy : {}'.format(data))
+    copy_text()
+    data = pyperclip.paste()
+    # print('data after copy : {}'.format(data))
+
+    data = data.lower()
+
+    # print('modified data before paste : {}'.format(data))
+    pyperclip.copy(data)
+    paste_text()
+    # print('modified data after paste : {}'.format(data))
+
+key_combos_with_functions = {
+    frozenset([Key.alt, KeyCode(char='u')]): convert_to_uppercase,
+    frozenset([Key.alt, KeyCode(char='l')]): convert_to_lowercase,
+}
+
+pressed_keys = set()
+
+def on_press(key):
+    pressed_keys.add(key)
+    # print("on_press: {}".format(pressed_keys))
+    if frozenset(pressed_keys) in key_combos_with_functions:
+        # print("Run things")
+        # print(key_combos_with_functions[frozenset(pressed_keys)])
+        key_combos_with_functions[frozenset(pressed_keys)]()
+
+def on_release(key):
+    pressed_keys.remove(key)
+    # print("on_release: {}".format(pressed_keys))
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
